95b3803721ad00cfda48dfb7ce4da55607de95c3
Lachance, Alexandre <alexandrelachance@me.com>
2023-11-22 07:55:34 -0500

    Complete compare

--- 3_duplicate_deletion/4_compare/compare.py
+++ 3_duplicate_deletion/4_compare/compare.py
@@ -2,12 +2,14 @@ from difflib import SequenceMatcher
 import json
 from tqdm import tqdm
 import csv
+import os
+import subprocess
 
 with open('../../1_collection/full.json') as f:
     repos = json.load(f)
-    repo_forge = {}
+    repo_info = {}
     for repo in repos:
-        repo_forge[repo['id']] = repo['forge']
+        repo_info[repo['id']] = (repo['forge'], repo['commit_ids'][0])
 
 
 def compare_files(path1, path2):
@@ -21,17 +23,32 @@ def compare_files(path1, path2):
 
 
 def get_all_relative_paths(dir):
+    paths = []
     for dir_path, dir_names, file_names in os.walk(dir):
         for file in file_names:
-            relative_path = os.path.relpath(os.path.join(dir_path, file), dir)
+            _, file_extension = os.path.splitext(os.path.join(dir_path, file))
+            path = os.path.join(dir_path, file)
+            if file_extension[1:] in ['p4', 'sh', 'py'] and not os.path.islink(path):
+                paths.append(os.path.relpath(path, dir))
+    return paths
+
+# Make sure the project is at latest commit
+def checkout_latest_commit(dir, id):
+    current_dir = os.getcwd()
+    os.chdir(dir)
+    subprocess.run(["git", "checkout", repo_info[id][1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    os.chdir(current_dir)
 
 
-def compare_directories(dir_1, dir_2):
+def compare_directories(dir_1, dir_2): 
     files_1 = get_all_relative_paths(dir_1)
     files_2 = get_all_relative_paths(dir_2)
 
     common_file_paths = list(set(files_1).intersection(files_2))
-    total_file_count = len(list(set(files_1).symmetric_difference(files_2))) + len(common_files)
+    total_file_count = len(list(set(files_1).symmetric_difference(files_2))) + len(common_file_paths)
+
+    if len(common_file_paths) == 0:
+        return 0.0
 
     total_score = 0.0
     for path in common_file_paths:
@@ -40,20 +57,22 @@ def compare_directories(dir_1, dir_2):
     return total_score / total_file_count
 
 def get_dir_path(repo_id):
-    return f'../../1_collection/{repo_forge[repo_id]}/repos/{repo_id}/'
+    return f'../../1_collection/{repo_info[repo_id][0]}/repos/{repo_id}/'
 
 
 def compare_projects(project_id_1, project_id_2):
-    return compare_directories(
-            get_dir_path(project_id_1),
-            get_dir_path(project_id_2)
-            )
+    dir_1 = get_dir_path(project_id_1)
+    dir_2 = get_dir_path(project_id_2)
+    checkout_latest_commit(dir_1, project_id_1)
+    checkout_latest_commit(dir_2, project_id_2)
+
+    return compare_directories(dir_1, dir_2)
 
 
 def compare_all():
     with open("../3_reduced_potential_pairs.json", 'r') as potential_pairs_file:
         potential_pairs = json.load(potential_pairs_file)
-    with open("scores.csv") as scores_file:
+    with open("scores.csv", mode='w', newline='') as scores_file:
         writer = csv.writer(scores_file)
         for pair in tqdm(potential_pairs):
             score = compare_projects(pair['id1'], pair['id2'])
---
+++ 3_duplicate_deletion/4_compare/dist.py
@@ -0,0 +1,22 @@
+import csv
+import matplotlib.pyplot as plt
+
+def generate_vis(scores):
+    # plt.hist(scores, bins=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], alpha=0.75, color='b', edgecolor='black')
+    plt.hist(scores, bins=10, alpha=0.75, color='b', edgecolor='black')
+
+    plt.xlabel('Scores')
+    plt.ylabel('Count')
+    plt.title('Similarity Scores')
+
+    plt.savefig('score_dist.png')
+
+scores = []
+
+with open('scores.csv', newline='') as scoresFile:
+    reader = csv.reader(scoresFile)
+    for row in reader:
+        scores.append(float(row[2]))
+        print(row[2])
+
+generate_vis(scores)
