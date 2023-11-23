7094d0365de1a99ddae75d6532d354dbbb351aad
Lachance, Alexandre <alexandrelachance@me.com>
2023-11-22 20:28:06 -0500

    Add vis

---
+++ 1_collection/dataset_comp_vis.py
@@ -0,0 +1,26 @@
+import json
+import matplotlib.pyplot as plt
+
+
+with open('full.json') as repoFiles:
+    repos = json.load(repoFiles)
+
+gitlab_count = 0
+github_count = 0
+for repo in repos:
+    if repo['forge'] == 'github':
+        github_count += 1
+    else:
+        gitlab_count += 1
+
+categories = ['GitHub', 'GitLab']
+values = [github_count, gitlab_count]
+print(values)
+
+plt.bar(categories, values, color=['blue', 'orange'])
+
+plt.xlabel('Forges')
+plt.ylabel('Number of Repos')
+plt.title('GitLab vs GitHub Repos')
+
+plt.savefig('forge_dist.png')
